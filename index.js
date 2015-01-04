f3301e53e5eac5a748287b50f79c60cd41a129da
careythelee <careythelee@users.noreply.github.com>
2015-01-04 05:44:29 +0000

    finish project

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-var express = require('express');
-var app = express();
+//var express = require('express');
+var express = require('express.io');
+var app = express().http().io();
+//var app = require('express.io')();
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var childProcess = require('child_process');
 var Gpio = require ('onoff').Gpio,
 	led = new Gpio(18, 'out');
+var data = null;
+
+
+app.listen(8080);
+//var io = require('socket.io').listen(server);
+//app.http().io();
+
+app.get('/switch',function(req,res){
+	if (led.readSync()){
+		res.end('on');
+	} else {
+		res.end('off');
+	} 
+});
 
 app.use(express.static(__dirname + "/public"));
 
+//io.sockets.on('connection', function(socket) {
+
+
+
 app.use(bodyParser.json());
 //app.use(body.urlencoded({extended:true}));
+/*
 app.use(multer());
 app.post('/pound',function(req,res){
 	console.log('post received');
@@ -20,19 +41,23 @@ app.post('/pound',function(req,res){
 	if (req.body.name == "Carey"){
 		childProcess.exec('omxplayer -o local testmusic.mp3', function(){});
 	}
-});
+})
+*/
 
-app.get('/switch',function(req,res){
-	if (led.readSync()){
-		res.end('on');
-	} else {
-		res.end('off');
-	} 
+app.io.route('ready', function(req){
+	console.log('new Visitor');
 });
 
 app.post('/switch',function(req,res){
 	flipLed();
 	console.log(req.param);
+	console.log (led.readSync());
+	if (led.readSync()){
+		data = true;
+	} else {
+		data = false;
+	} 
+	app.io.broadcast('notification', data);
 });
 
 var flipLed = function () {
@@ -41,6 +66,7 @@ var flipLed = function () {
 	led.writeSync(newLedValue);
 };
 
+/*
 var postblink = function(){
 	var timer = setInterval(flipLed, 500);
 	setTimeout(function(){
@@ -48,9 +74,13 @@ var postblink = function(){
 		led.writeSync(0);
 	}, 5000);	
 };
+*/
 
 
-app.listen(8080);
+
+
+
+
 
 process.on('SIGINT', function(){
 	led.unexport();
